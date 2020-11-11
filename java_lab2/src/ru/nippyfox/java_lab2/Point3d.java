32157952c217d5f628e3371708e237a26de5a223
iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>
2020-11-11 13:58:34 +0300

    Make an extend

--- java_lab2/src/ru/nippyfox/java_lab2/Point3d.java
+++ java_lab2/src/ru/nippyfox/java_lab2/Point3d.java
@@ -1,44 +1,21 @@
 package ru.nippyfox.java_lab2;
 
-public class Point3d {
-    /* координата X */
-    private double xCoord;
-    /* координата Y */
-    private double yCoord;
+public class Point3d extends Point2d {
     /* координата Z */
     private double zCoord;
     /* конструктор инициализации */
-    public Point3d (double x, double y, double z) {
-        xCoord = x;
-        yCoord = y;
+    public Point3d (double z) {
         zCoord = z;
     }
     /* Конструктор по умолчанию. */
     public Point3d () {
-        //Вызовите конструктор с двумя параметрами и определите источник.
-        this(0, 0, 0);
+        this(0);
     }
-    /* Возвращение координаты X */
-    public double getX () {
-        return xCoord;
-    }
-    /* Возвращение координаты Y */
-    public double getY () {
-        return yCoord;
-    }
-    /* Возвращение координаты Y */
+    /* Возвращение координаты Z */
     public double getZ () {
         return zCoord;
     }
-    /* Установка значения координаты X. */
-    public void setX (double val) {
-        xCoord = val;
-    }
-    /* Установка значения координаты Y. */
-    public void setY (double val) {
-        yCoord = val;
-    }
-    /* Установка значения координаты Y. */
+    /* Установка значения координаты Z. */
     public void setZ (double val) {
         zCoord = val;
     }
