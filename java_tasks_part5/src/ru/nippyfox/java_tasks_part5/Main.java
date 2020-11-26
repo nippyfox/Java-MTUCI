ae733834eb3a77f878e956b31454d0d540f5e5e4
iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>
2020-11-27 00:02:12 +0300

    Finish 5.2-5.3

--- java_tasks_part5/src/ru/nippyfox/java_tasks_part5/Main.java
+++ java_tasks_part5/src/ru/nippyfox/java_tasks_part5/Main.java
@@ -27,6 +27,60 @@ public class Main {
         return res;
     }
 
+    public static boolean canMove(String a, String bStr, String cStr) { // 5.2
+        char[] b = bStr.toCharArray();
+        char[] c = cStr.toCharArray();
+        if ((b.length == 2 && c.length == 2) && ((b[0] >= 'A' && b[0] <= 'H') || (b[0] >= 'a' && b[0] <= 'h')) &&
+                ((c[0] >= 'A' && c[0] <= 'H') || (c[0] >= 'a' && c[0] <= 'h')) && (b[1] >= '1' && b[1] <= '8')
+                && (c[1] >= '1' && c[1] <= '8')) {
+            if (a.trim().toLowerCase().equals("пешка")) {
+                if (b[0] == c[0]) {
+                    if (b[1] == '1') {
+                        return false;
+                    }
+                    if (b[1] == '2' && c[1] == '4') {
+                        return true;
+                    }
+                    int begin = (int) b[1];
+                    int end = (int) c[1];
+                    return begin + 1 == end;
+                } else {
+                    return false;
+                }
+            } else if (a.trim().toLowerCase().equals("конь")) {
+                return ((Math.abs((b[1] - c[1])) == 2 && Math.abs(b[0] - c[0]) == 1) ||
+                        (Math.abs((b[0] - c[0])) == 2 && Math.abs(b[1] - c[1]) == 1));
+            } else if (a.trim().toLowerCase().equals("слон")) {
+                return (Math.abs(b[0] - c[0]) == Math.abs(b[1] - c[1]));
+            } else if (a.trim().toLowerCase().equals("ладья")) {
+                return b[0] == c[0] || b[1] == c[1];
+            } else if (a.trim().toLowerCase().equals("ферзь")) {
+                return (b[0] == c[0] || b[1] == c[1]) || (Math.abs(b[0] - c[0]) == Math.abs(b[1] - c[1]));
+            } else if (a.trim().toLowerCase().equals("король")) {
+                return ((b[0] == c[0]) && (b[1] + 1 == c[1]) || (b[1] == c[1]) && (b[0] + 1 == c[0]) ||
+                        (b[0] == c[0]) && (b[1] - 1 == c[1]) || (b[1] == c[1]) && (b[0] - 1 == c[0]));
+            } else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean canComplete(String a, String b) { // 5.3
+        int lastSymPos = -1;
+        for (char let: a.toCharArray()) {
+            int curSymPos = b.indexOf((int)let);
+            if ((curSymPos != -1)) {
+                lastSymPos = curSymPos;
+                b = b.substring(curSymPos+1, b.length());
+            }
+            else return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.ENGLISH);
         Scanner sc = new Scanner(System.in);
@@ -62,6 +116,21 @@ public class Main {
             }
             case 42 -> {
                 System.out.println("canMove()");
+                System.out.println("Введите фигуру:");
+                String a2 = sc.nextLine();
+                System.out.println("Введите положение фигуры на шахматной доске:");
+                String b2 = sc.nextLine();
+                System.out.println("Введите целевую позицию:");
+                String c2 = sc.nextLine();
+                System.out.println("Результат: " + canMove(a2, b2, c2));
+            }
+            case 43 -> {
+                System.out.println("canComplete()");
+                System.out.println("Введите подстроку:");
+                String a3 = sc.nextLine();
+                System.out.println("Введите входную строку:");
+                String b3 = sc.nextLine();
+                System.out.println("Результат: " + canComplete(a3, b3));
             }
         }
     }
